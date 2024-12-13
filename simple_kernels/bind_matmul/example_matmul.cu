87b30649818d93ecae61827ef4470545cfd85cc1
Simran Arora <emailsimran@gmail.com>
2024-12-12 17:34:26 -0800

    updates

--- simple_kernels/bind_matmul/example_matmul.cu
+++ simple_kernels/bind_matmul/example_matmul.cu
@@ -2,102 +2,107 @@
 #include "pyutils/pyutils.cuh"
 using namespace kittens;
 
-#define NUM_THREADS (kittens::WARP_THREADS) // use 1 warp
+#define NUM_WORKERS (1) // use 1 warp for this example
+#define NUM_THREADS (NUM_WORKERS * kittens::WARP_THREADS)  // number of threads is num_workers * num_threads_per_worker (32)
 
-#define _row 16
-#define _col 32
+// MODEL TODO: SET DIMENSIONS OF INPUTS
+#define _row 16 // rows per input matrix x
+#define _col 16 // columns per input matrix x
 
-// input is bfloat16, output is float32
-// NEED TO CHECK IF THIS IS CORRECT
-using x_gl  = gl<bf16, -1, -1, -1, -1, st_bf<_row, _col>>;
-using o_gl  = gl<float, -1, -1, -1, -1, st_fl<_row, _row>>;
+/* 
+MODEL TODO: DEFINE GLOBAL MEMORY DESCRIPTORS
+gl: indicates global layout
+bf16: indicates the data type
+dimmensions: {batch, head, depth, width} for global tensors (-1 means runtime dimension, non-negative means compile-time dimension)
+st: when loading from global tensor at some {b, h, d, w} index, this is the shape of the tile that will be loaded to shared memory
+*/
+using x_gl  = gl<bf16, -1, -1, -1, -1,  st<bf16, _row, _col>>;  // input is bfloat16
+using o_gl  = gl<float, -1, -1, -1, -1, st<float, _row, _row>>; // output is float32
 
+/*
+CREATE GLOBAL MEMORY DESCRIPTORS
+CREATE GRID AND BLOCK LAUNCH DIMENSIONS
+*/
 struct micro_globals {
-    x_gl x;
+    x_gl x, y;
     o_gl o;
-    dim3 grid()  { return dim3(o.batch, o.depth, o.rows); }
-    dim3 block() { return dim3(o.cols); }
+    dim3 grid()  { return dim3(o.depth, o.batch); } // dimensions we parallelize over (e.g., batches, heads)
+    dim3 block() { return dim3(NUM_THREADS); } // number of threads per threadblock
     size_t dynamic_shared_memory() { return 224000; } // I added this but kinda sus
     
 };
 
-// make this number of threads in a thread block
+/*
+ACTUAL CUDA KERNEL
+1. Define shared memory allocator and tiles
+2. Define register memory
+3. Load from global to shared memory using {b, h, d, w} indexing
+4. Load from shared to register memory
+5. Do the work on tiles
+6. Store from register to shared memory
+7. Store from shared to global memory using {b, h, d, w} indexing
+*/
 __global__ __launch_bounds__(NUM_THREADS, 1)
 void micro_tk(const __grid_constant__ micro_globals g) {
+    // get current position in grid
+    int head = blockIdx.x;
+    int batch = blockIdx.y;
+
     // shared memory
     extern __shared__ alignment_dummy __shm[];
     shared_allocator al((int*)&__shm[0]);
     st_bf<_row, _col> (&x_s) = al.allocate<st_bf<_row, _col>>(); // bf16 tiles
+    st_bf<_row, _col> (&y_s) = al.allocate<st_bf<_row, _col>>(); // bf16 tiles
     st_fl<_row, _row> (&o_s) = al.allocate<st_fl<_row, _row>>(); // float tiles
 
     // register memory
     rt_bf<_row, _col> x_reg; // bf16 register
+    rt_bf<_row, _col> y_reg; // bf16 register
     rt_fl <_row, _row> accum_tile;  
     zero(accum_tile);
 
     // load from HBM to shared
-    load(x_s, g.x, {0, 0, 0, 0});
+    load(x_s, g.x, {batch, head, 0, 0});
+    load(y_s, g.y, {batch, head, 0, 0});
     __syncthreads();
 
     // load from shared to register
     load(x_reg, x_s);
+    load(y_reg, y_s);
     __syncthreads();
 
     // now do the matmul and accumulate to accum_tile
-    mma_ABt(accum_tile, x_reg, x_reg, accum_tile); // o = torch.matmul(x, x.transpose(1, 2))
+    mma_ABt(accum_tile, x_reg, y_reg, accum_tile); // o = torch.matmul(x, x.transpose(1, 2))
     __syncthreads();
 
     // store from register to shared
     store(o_s, accum_tile);
     __syncthreads();
 
-    one(o_s);
     // store from shared to HBM
-    store(g.o, o_s, {0, 0, 0, 0});
+    store(g.o, o_s, {batch, head, 0, 0});
     __syncthreads();
 }
 
-
-// // old code
-// void dispatch_micro( bf16 *d_x, float *d_o ) {
-//     using x_gl = gl<bf16, -1, -1, -1, -1, st_bf<_row, _col>>;
-//     using o_gl = gl<float, -1, -1, -1, -1, st_fl<_row, _row>>;
-//     using globals = micro_globals;
-//     x_gl  x_arg{d_x, 1, 1, _row, _col};
-//     o_gl  o_arg{d_o, 1, 1, _row, _row};
-//     globals g{x_arg, o_arg};
-//     unsigned long mem_size = 50480; 
-//     cudaFuncSetAttribute(
-//         micro_tk,
-//         cudaFuncAttributeMaxDynamicSharedMemorySize,
-//         mem_size
-//     );
-//     micro_tk<<<1,32,mem_size>>>(g);
-//     cudaDeviceSynchronize();
-// }
-
-
+/*
+DISPATCH FUNCTION 
+*/
 void dispatch_micro(micro_globals g) {
-    // MISSING??
-    // I THINK WE NEED TO REDO THE LAYOUT!
-    // somehow do the step? _gl  x_arg{d_x, 1, 1, _row, _col};
-    // i tried to put another TK layout here but it didn't work
-
     unsigned long mem_size = 50480; 
     cudaFuncSetAttribute(
         micro_tk,
         cudaFuncAttributeMaxDynamicSharedMemorySize,
         mem_size
     );
-    // micro_tk<<<1,32,mem_size>>>(g);
     micro_tk<<<g.grid(), g.block(), mem_size>>>(g);
-    // micro_tk<<<1,32, mem_size>>>(g);
     cudaDeviceSynchronize();
 }
 
-
+/* 
+PYTHON BINDINGS
+*/
 PYBIND11_MODULE(simple_tk, m) {
     m.doc() = "simple_tk python module";
-    BIND_KERNEL(m, "micro_tk", micro_tk, micro_globals, x, o); // For wrapping kernels directly.
-    BIND_FUNCTION(m, "dispatch_micro", dispatch_micro, micro_globals, x, o); // For host functions that wrap the kernel.
+    BIND_KERNEL(m, "micro_tk", micro_tk, micro_globals, x, y, o); // For wrapping kernels directly.
+    BIND_FUNCTION(m, "dispatch_micro", dispatch_micro, micro_globals, x, y, o); // For host functions that wrap the kernel.
 }
--- simple_kernels/bind_matmul/run_from_python.py
+++ simple_kernels/bind_matmul/run_from_python.py
@@ -2,8 +2,9 @@ import simple_tk
 import torch
 
 B = 1
+H = 4
 N = 16
-D = 32
+D = 16
 
 """
 Reference Logic: https://github.com/HazyResearch/ThunderKittens/blob/tk_gen/simple_kernels/micro_add/
@@ -12,28 +13,28 @@ Reference Logic: https://github.com/HazyResearch/ThunderKittens/blob/tk_gen/simp
 
 # input = torch.ones((B, N, D), dtype=torch.bfloat16, device='cuda')
 
-input = torch.ones((B, N, D), dtype=torch.bfloat16, device='cuda')
-print("Input tensor:", input.mean().item(), "dtype:", input.dtype)  # Debug input
+input_x = torch.ones((B, H, N, D), dtype=torch.bfloat16, device='cuda')
+input_y = torch.ones((B, H, N, D), dtype=torch.bfloat16, device='cuda') * 2 
+print("Input tensor:", input_x.mean().item(), "dtype:", input_x.dtype) 
 
-def matmul(x):
+def matmul(x, y):
     """
     o = x @ x.T
     input: x is bfloat16
     output: o is float32 (output accumulator)
     """
-    o = torch.matmul(x, x.transpose(1, 2)).to(torch.float32)
-    # print("Result tensor:", result.mean().item(), "dtype:", result.dtype)  # Debug output
+    o = torch.matmul(x, y.transpose(-2, -1)).to(torch.float32)
     return o
 
-output_ref = matmul(input)
+output_ref = matmul(input_x, input_y)
 print("Ref output mean:", output_ref.mean().item())  # Debug final output
 
-# input_copy = input.clone()
-input_copy = torch.ones((B, N, D), dtype=torch.bfloat16, device='cuda')
+input_x_copy = input_x.clone()
+input_y_copy = input_y.clone()
 
-output_tk = torch.zeros((16, 16), dtype=torch.float32, device='cuda')
+output_tk = torch.zeros((B, H, N, N), dtype=torch.float32, device='cuda')
 
-simple_tk.dispatch_micro(input_copy, output_tk)
+simple_tk.dispatch_micro(input_x_copy, input_y_copy, output_tk)
 print("TK tensor mean:", output_tk.mean().item(), "dtype:", output_tk.dtype)  # Debug output
 
 print(output_tk)

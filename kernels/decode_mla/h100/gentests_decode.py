de92d5f1af3d3307f70844131f673858a9a41479
Aaryan0404 <aaryan0404@gmail.com>
2025-02-07 16:50:39 -0800

    ckpt

--- kernels/decode_mla/h100/gentests_decode.py
+++ kernels/decode_mla/h100/gentests_decode.py
@@ -24,6 +24,8 @@ torch.random.manual_seed(42)
 # QK Dimension(s).
 B, R, H, Z, Zc, Zr = 4, 0, 16, 576, 512, 64
 
+# use sdpa to do multi-query gentests
+
 # Input Dimension(s).
 L = B * (R + 1)
 D = 7168
@@ -32,24 +34,28 @@ D = 7168
 Sz = 1 / sqrt(Z)
 
 # Latent Cache Specific(s).
-N, K = 128, 256
+# N, K = 128, 256
+N, K   = 128, 128
 
 # Sequence Length Specific(s).
 T = 32768 // K
-Sl, Sh = 32, 1024
+
+# Sl, Sh = 32, 1024
+Sl, Sh   = 128, 129
 
 with torch.device(DEVICE):
 
     # Initialize Input(s) and Cache(s).
     hidden = torch.randn(L, D, dtype=DTYPE)
-    cache = torch.empty(N, K, 1, Z, dtype=DTYPE)
+    cache  = torch.empty(N, K, 1, Z, dtype=DTYPE)
 
     mitchell(weight := torch.empty(H * Z, D, dtype=DTYPE))
 
-    query = (
-        linear(hidden, weight)
-        .view(B, R + 1, H, Z)
-    )
+    # query = (
+    #     linear(hidden, weight)
+    #     .view(B, R + 1, H, Z)
+    # )
+    query = torch.ones(B, R + 1, H, Z, dtype=DTYPE)
 
     # Define Sequence Length and Block Size(s).
     lengths = torch.randint(Sl, Sh, (B,), dtype=torch.int32)
@@ -77,7 +83,7 @@ with torch.device(DEVICE):
     )
 
     # Prefill Latent Cache & Extract Padded.
-    latent   = torch.randn(total, 1, Z, dtype=DTYPE)
+    latent   = torch.ones(total, 1, Z, dtype=DTYPE)
     expanded = latent.expand(total, H, Z)
 
     sequence_ids, position_ids = (
@@ -90,7 +96,7 @@ with torch.device(DEVICE):
     padded_key   = torch.zeros(B, maximum, H, Z, dtype=DTYPE)
     padded_value = torch.zeros(B, maximum, H, Zc, dtype=DTYPE)
 
-    padded_key[sequence_ids, position_ids] = expanded
+    padded_key[sequence_ids, position_ids]   = expanded
     padded_value[sequence_ids, position_ids] = expanded[..., :Zc]
 
     entry_ids  = table[sequence_ids, position_ids.floor_divide(K)]
@@ -116,23 +122,18 @@ with torch.device(DEVICE):
 # execution barrier
 torch.cuda.current_stream().synchronize()
 
-def naive_sdpa(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, *, scale: float) -> torch.Tensor:
+padded_o1 = sdpa(
+    query=query.transpose(1, 2),
+    key=padded_key.transpose(1, 2),
+    value=padded_value.transpose(1, 2),
+    attn_mask=mask,
+    dropout_p=0.0,
+    is_causal=False,
+    scale=Sz,
+    enable_gqa=False,
+)
 
-    scores = torch.einsum(
-        '... s z , ... t z -> ... s t',
-        q.transpose(1, 2),
-        k.transpose(1, 2),
-    )
-
-    out = torch.einsum(
-        '... s t , ... t z -> ... s z',
-        scores.mul_(scale).softmax(dim=-1),
-        v.transpose(1, 2),
-    )
-
-    return out.transpose(1, 2)
-
-output = naive_sdpa(query, padded_key, padded_value, scale=Sz)
+o1 = padded_o1.transpose(1, 2)
 torch.cuda.current_stream().synchronize()
 
 # inputs
@@ -144,7 +145,45 @@ torch.cuda.current_stream().synchronize()
 # outputs
 ### output
 
-breakpoint()
+print("--------------------------------------")
+print("--------------------------------------")
+print("Q       shape: ", query.shape)
+print("Cache   shape: ", cache.shape)
+print("Lengths shape: ", lengths.shape)
+print("Table   shape: ", table.shape)
+print("Output  shape: ", o1.shape)
+print("--------------------------------------")
+print("--------------------------------------")
+
+filename = f'ones_B{B}_R{R}_H{H}_Z{Z}_Zc{Zc}_Zr{Zr}_N{N}_K{K}_Sl{Sl}_Sh{Sh}.txt'
+
+with open(filename, 'w') as f:
+    qf       = query.to(torch.float32).flatten().detach().cpu().numpy()
+    cachef   = cache.to(torch.float32).flatten().detach().cpu().numpy()
+    lengthsf = lengths.to(torch.float32).flatten().detach().cpu().numpy()
+    tablef   = table.to(torch.float32).flatten().detach().cpu().numpy()
+    
+    outputf  = o1.to(torch.float32).flatten().detach().cpu().numpy()
+    
+    for i in trange(len(qf)):
+        f.write(repr(float(qf[i])))
+        f.write(' ')
+    
+    for i in trange(len(cachef)):
+        f.write(repr(float(cachef[i])))
+        f.write(' ')
+    
+    for i in trange(len(lengthsf)):
+        f.write(repr(float(lengthsf[i])))
+        f.write(' ')
+    
+    for i in trange(len(tablef)):
+        f.write(repr(float(tablef[i])))
+        f.write(' ')
+    
+    for i in trange(len(outputf)):
+        f.write(repr(float(outputf[i])))
+        f.write(' ')
 
 
 
--- kernels/decode_mla/h100/h100.cu
+++ kernels/decode_mla/h100/h100.cu
@@ -14,88 +14,117 @@ template<int D> struct mla_decode_tile_dims {};
 template<> struct mla_decode_tile_dims<576> {
     constexpr static int tile_width_dl = (512);
     constexpr static int tile_width_dr = (64); 
+    constexpr static int tile_width_ml = (576);
 
     constexpr static int new_height   = (16);
-    constexpr static int cache_height = (8*16);
+    constexpr static int cache_height = (128);
 
-    constexpr static int stages     = (1);
-
+    constexpr static int stages     = (1); 
     constexpr static int kv_blocks  = stages;
     constexpr static int kv_split   = cache_height * kv_blocks;
 };
 
-template<int D> struct fwd_globals {
-    using q_tile         = st_bf<fwd_attend_ker_tile_dims<D>::new_height,   fwd_attend_ker_tile_dims<D>::tile_width>;
-    using k_cache_tile   = st_bf<fwd_attend_ker_tile_dims<D>::cache_height, fwd_attend_ker_tile_dims<D>::tile_width>;
-    using v_cache_tile   = st_bf<fwd_attend_ker_tile_dims<D>::cache_height, fwd_attend_ker_tile_dims<D>::tile_width>;
-    using k_seqlens_tile = st_bf<fwd_attend_ker_tile_dims<D>::new_height,   fwd_attend_ker_tile_dims<D>::tile_width>;
-    using o_tile         = st_bf<fwd_attend_ker_tile_dims<D>::new_height,   fwd_attend_ker_tile_dims<D>::tile_width>;
+template<int D> struct decode_globals {
+    using q1_tile = st_bf<mla_decode_tile_dims<576>::new_height, mla_decode_tile_dims<576>::tile_width_dl>;
+    using q2_tile = st_bf<mla_decode_tile_dims<576>::new_height, mla_decode_tile_dims<576>::tile_width_dr>;
+    using k1_tile = st_bf<mla_decode_tile_dims<576>::new_height, mla_decode_tile_dims<576>::tile_width_dl>;
+    using k2_tile = st_bf<mla_decode_tile_dims<576>::new_height, mla_decode_tile_dims<576>::tile_width_dr>;
 
-    using q_gl         = gl<bf16, -1, -1, -1, -1, q_tile>;
-    using k_cache_gl   = gl<bf16, -1, -1, -1, -1, k_cache_tile>;
-    using v_cache_gl   = gl<bf16, -1, -1, -1, -1, v_cache_tile>;
-    using k_seqlens_gl = gl<bf16, -1, -1, -1, -1, k_seqlens_tile>;
-    using o_gl         = gl<bf16, -1, -1, -1, -1, o_tile>;
+    using v_tile  = st_bf<mla_decode_tile_dims<576>::new_height, mla_decode_tile_dims<576>::tile_width_dl>;
+    using o_tile  = st_bf<mla_decode_tile_dims<576>::new_height, mla_decode_tile_dims<576>::tile_width_dl>;
 
-    q_gl         q;
-    k_cache_gl   k_cache;
-    v_cache_gl   v_cache;
-    k_seqlens_gl k_seqlens;
-    o_gl         o;
+    using partial_tile = st_bf<mla_decode_tile_dims<576>::new_height, 64>; 
+    using q_gl = gl<bf16, -1, -1, -1, -1, kittens::ducks::tma::descriptor<partial_tile, 1>>;
+    using l_gl = gl<bf16, -1, -1, -1, -1, kittens::ducks::tma::descriptor<partial_tile, 1>>;
+    using o_gl = gl<bf16, -1, -1, -1, -1, kittens::ducks::tma::descriptor<partial_tile, 1>>;
 
-    const int N;
-    const int hr;
+    q_gl q;
+    l_gl l;
+    o_gl o;
+
+    float *lengths;
+    float *table;
+
+    const int new_tokens;
+    const int page_size;
+    const float softmax_scale;
 };
 
-template<int D, bool is_causal>
+// gl<bf16, -1, -1, -1, -1, tile>;
+// gl<bf16, -1, -1, -1, -1, kittens::ducks::tma::descriptor<tile, 1>>;
+
+template<int D>
 __global__  __launch_bounds__((NUM_WORKERS)*kittens::WARP_THREADS, 1)
-void fwd_attend_ker(const __grid_constant__ fwd_globals<D> g) {
+void decode_kernel(const __grid_constant__ fwd_globals<D> g) {
     extern __shared__ int __shm[]; 
     tma_swizzle_allocator al((int*)&__shm[0]);
     int warpid = kittens::warpid(), warpgroupid = warpid/kittens::WARPGROUP_WARPS;
 
-    using K = fwd_attend_ker_tile_dims<D>;
+    using K = mla_decode_tile_dims<D>;
 
-    using q_tile    =         st_bf<K::qo_height, K::tile_width>;
-    using k_tile    =         st_bf<K::kv_height, K::tile_width>;
-    using v_tile    =         st_bf<K::kv_height, K::tile_width>;
-    using l_col_vec = col_vec<st_fl<K::qo_height, K::tile_width>>;
-    using o_tile    =         st_bf<K::qo_height, K::tile_width>;
-    
-    q_tile (&q_smem)[CONSUMER_WARPGROUPS] = al.allocate<q_tile, CONSUMER_WARPGROUPS>();
-    k_tile (&k_smem)[K::stages]           = al.allocate<k_tile, K::stages          >();
-    v_tile (&v_smem)[K::stages]           = al.allocate<v_tile, K::stages          >();
-    auto   (*o_smem)                      = reinterpret_cast<o_tile(*)>(q_smem);
+    using q1_tile = st_bf<mla_decode_tile_dims<D>::new_height,   mla_decode_tile_dims<D>::tile_width_dl>;
+    using q2_tile = st_bf<mla_decode_tile_dims<D>::new_height,   mla_decode_tile_dims<D>::tile_width_dr>;
+    using k1_tile = st_bf<mla_decode_tile_dims<D>::cache_height, mla_decode_tile_dims<D>::tile_width_dl>;
+    using k2_tile = st_bf<mla_decode_tile_dims<D>::cache_height, mla_decode_tile_dims<D>::tile_width_dr>;
+    using o_tile  = st_bf<mla_decode_tile_dims<D>::new_height,   mla_decode_tile_dims<D>::tile_width_dl>;
 
-    int kv_head_idx       = blockIdx.y / g.hr;
-    int global_kv_seq_idx = blockIdx.x * K::kv_blocks;
+    using partial_tile = st_bf<mla_decode_tile_dims<D>::new_height, 64>; 
+
+    q1_tile (&q1_smem)[1] = al.allocate<q1_tile, 1>();
+    q2_tile (&q2_smem)[1] = al.allocate<q2_tile, 1>();
+
+    k1_tile (&k1_smem)[K::stages] = al.allocate<k1_tile, K::stages>();
+    k2_tile (&k2_smem)[K::stages] = al.allocate<k2_tile, K::stages>();
+
+    o_tile  (&o_smem) = reinterpret_cast<o_tile(*)>(q1_smem);
+
+    int kv_head_idx       = blockIdx.y;
+    int global_kv_seq_idx = blockIdx.x;
 
     auto ZERO    = kittens::base_types::constants<bf16>::zero();
     auto NEG_INF = kittens::base_types::constants<bf16>::neg_infty();
 
-    __shared__ kittens::semaphore qsmem_semaphore, k_smem_arrived[K::stages], v_smem_arrived[K::stages], compute_done[K::stages];
+    int batch_len = g.lengths[blockIdx.z];
+    if (global_kv_seq_idx * g.page_size >= batch_len) {
+        return;
+    }
+
+    int page_idx = g.table[(blockIdx.z * T) + blockIdx.x];
+
+    __shared__ kittens::semaphore qsmem1_semaphore, qsmem2_semaphore; 
+    __shared__ kittens::semaphore k1_smem_arrived[K::stages], k2_smem_arrived[K::stages]; 
+    // compute_done[K::stages];
     if (threadIdx.x == 0) { 
-        init_semaphore(qsmem_semaphore, 0, 1);
+        init_semaphore(qsmem1_semaphore, 0, 1);
+        init_semaphore(qsmem2_semaphore, 0, 1);
         for(int j = 0; j < K::stages; j++) {
-            init_semaphore(k_smem_arrived[j], 0, 1);
-            init_semaphore(v_smem_arrived[j], 0, 1);
-            init_semaphore(compute_done[j], CONSUMER_WARPGROUPS, 0);
+            init_semaphore(k1_smem_arrived[j], 0, 1);
+            init_semaphore(k2_smem_arrived[j], 0, 1);
         }
 
-        tma::expect_bytes(qsmem_semaphore, sizeof(q_smem));
+        tma::expect_bytes(qsmem1_semaphore, sizeof(q1_smem)); 
+        tma::expect_bytes(qsmem2_semaphore, sizeof(q2_smem)); 
 
-        for (int wg = 0; wg < CONSUMER_WARPGROUPS; wg++) {
-            coord<q_tile> q_tile_idx = {blockIdx.z, blockIdx.y, wg, 0};
-            tma::load_async(q_smem[wg], g.q, q_tile_idx, qsmem_semaphore, NEG_INF); 
+        int i; 
+        coord<partial_tile> q_tile_idx; 
+        for (i = 0; i < (512/64); i++) {
+            q_tile_idx = {blockIdx.z, 0, blockIdx.y, i};
+            auto &q_subtile = subtile_inplace<16, 64>(q1_smem[0], {0, i});
+            tma::load_async<1>(q_subtile, g.q, q_tile_idx, qsmem1_semaphore, NEG_INF);
         }
+        q_tile_idx = {blockIdx.z, 0, blockIdx.y, i};
+        tma::load_async<1>(q2_smem[0], g.q, q_tile_idx, qsmem2_semaphore, NEG_INF);
 
         for (int j = 0; j < K::stages; j++) {
-            // k_seqlen - get batch using blockIdx.y - compute seq_len offset from padding
-            coord<k_tile> kv_tile_idx = {blockIdx.z, kv_head_idx, global_kv_seq_idx + j, 0};
-            tma::expect_bytes(k_smem_arrived[j], sizeof(k_tile));
-            tma::expect_bytes(v_smem_arrived[j], sizeof(v_tile));
-            tma::load_async(k_smem[j], g.k, kv_tile_idx, k_smem_arrived[j], NEG_INF);
-            tma::load_async(v_smem[j], g.v, kv_tile_idx, v_smem_arrived[j], ZERO);
+            int i; 
+            coord<partial_tile> kv_tile_idx;
+            for (i = 0; i < (512/64); i++) {
+                kv_tile_idx     = {page_idx, j, 0, i};
+                auto &k_subtile = subtile_inplace<128, 64>(k1_smem[j], {0, i});
+                tma::load_async<1>(k_subtile, g.k, kv_tile_idx, k1_smem_arrived[j], NEG_INF);
+            }
+            kv_tile_idx = {page_idx, j, 0, i};
+            tma::load_async<1>(k2_smem[0], g.k, kv_tile_idx, k2_smem_arrived[j], NEG_INF);
         }
     }
     __syncthreads();
@@ -105,24 +134,25 @@ void fwd_attend_ker(const __grid_constant__ fwd_globals<D> g) {
     if(warpgroupid == NUM_WARPGROUPS-1) {
         warpgroup::decrease_registers<32>();
         
-        int kv_iters; 
-        if constexpr (is_causal) {
-            kv_iters = (seq_idx *   (K::qo_height/kittens::TILE_ROW_DIM<bf16>)) - 1 + (CONSUMER_WARPGROUPS * (K::qo_height/kittens::TILE_ROW_DIM<bf16>)); 
-            kv_iters = ((kv_iters / (K::kv_height/kittens::TILE_ROW_DIM<bf16>)) == 0) ? (0) : ((kv_iters /   (K::kv_height/kittens::TILE_ROW_DIM<bf16>)) - 1);
-        }
-        else { kv_iters = kv_blocks-2; }
+        // int kv_iters; 
+        // if constexpr (is_causal) {
+        //     kv_iters = (seq_idx *   (K::qo_height/kittens::TILE_ROW_DIM<bf16>)) - 1 + (CONSUMER_WARPGROUPS * (K::qo_height/kittens::TILE_ROW_DIM<bf16>)); 
+        //     kv_iters = ((kv_iters / (K::kv_height/kittens::TILE_ROW_DIM<bf16>)) == 0) ? (0) : ((kv_iters /   (K::kv_height/kittens::TILE_ROW_DIM<bf16>)) - 1);
+        // }
+        // else { kv_iters = kv_blocks-2; }
+        int kv_iters = 1; 
 
-        if(warpid == NUM_WORKERS-4) {
-            for (auto kv_idx = pipe_idx - 1; kv_idx <= kv_iters; kv_idx++) {
-                coord<k_tile> kv_tile_idx = {blockIdx.z, kv_head_idx, kv_idx + 1, 0};
-                tma::expect_bytes(k_smem_arrived[(kv_idx+1)%K::stages], sizeof(k_tile));
-                tma::expect_bytes(v_smem_arrived[(kv_idx+1)%K::stages], sizeof(v_tile));
-                tma::load_async(k_smem[(kv_idx+1)%K::stages], g.k, kv_tile_idx, k_smem_arrived[(kv_idx+1)%K::stages]);
-                tma::load_async(v_smem[(kv_idx+1)%K::stages], g.v, kv_tile_idx, v_smem_arrived[(kv_idx+1)%K::stages]);
+        // if(warpid == NUM_WORKERS-4) {
+        //     for (auto kv_idx = pipe_idx - 1; kv_idx <= kv_iters; kv_idx++) {
+        //         coord<k_tile> kv_tile_idx = {blockIdx.z, kv_head_idx, kv_idx + 1, 0};
+        //         tma::expect_bytes(k_smem_arrived[(kv_idx+1)%K::stages], sizeof(k_tile));
+        //         tma::expect_bytes(v_smem_arrived[(kv_idx+1)%K::stages], sizeof(v_tile));
+        //         tma::load_async(k_smem[(kv_idx+1)%K::stages], g.k, kv_tile_idx, k_smem_arrived[(kv_idx+1)%K::stages]);
+        //         tma::load_async(v_smem[(kv_idx+1)%K::stages], g.v, kv_tile_idx, v_smem_arrived[(kv_idx+1)%K::stages]);
                 
-                wait(compute_done[(kv_idx)%K::stages], (kv_idx/K::stages)%2);
-            }
-        }
+        //         wait(compute_done[(kv_idx)%K::stages], (kv_idx/K::stages)%2);
+        //     }
+        // }
     }
     else {
         warpgroup::increase_registers<160>();
@@ -138,18 +168,20 @@ void fwd_attend_ker(const __grid_constant__ fwd_globals<D> g) {
         zero(o_reg);
 
         int kv_iters; 
-        if constexpr (is_causal) {
-            kv_iters = (seq_idx * 4) - 1 + (CONSUMER_WARPGROUPS * 4);
-            kv_iters = (kv_iters/8);
-        }
-        else { kv_iters = kv_blocks - 1; }
+        // if constexpr (is_causal) {
+        //     kv_iters = (seq_idx * 4) - 1 + (CONSUMER_WARPGROUPS * 4);
+        //     kv_iters = (kv_iters/8);
+        // }
+        // else { kv_iters = kv_blocks - 1; }
+        kv_iters = 1; // ATTN_K/st_bf<mla_decode_tile_dims<D>::cache_height
 
         wait(qsmem_semaphore, 0);
 
         for (auto kv_idx = 0; kv_idx <= kv_iters; kv_idx++) {
         
             wait(k_smem_arrived[(kv_idx)%K::stages], (kv_idx/K::stages)%2);
-            warpgroup::mm_ABt(att_block, q_smem[warpgroupid], k_smem[(kv_idx)%K::stages]);
+            warpgroup::mm_ABt(att_block,  q1_smem[0], k1_smem[(kv_idx)%K::stages]);
+            warpgroup::mma_ABt(att_block, q2_smem[0], k2_smem[(kv_idx)%K::stages]);
             
             copy(max_vec_last_scaled, max_vec);
             if constexpr (D == 64) { mul(max_vec_last_scaled, max_vec_last_scaled, 1.44269504089f*0.125f); }
@@ -157,34 +189,43 @@ void fwd_attend_ker(const __grid_constant__ fwd_globals<D> g) {
             
             warpgroup::mma_async_wait();
 
-            if constexpr (is_causal) {
-                const int q_blk = (seq_idx * (K::qo_height/kittens::TILE_ROW_DIM<bf16>)) + warpid; 
-                      int k_blk = (kv_idx  * (K::kv_height/kittens::TILE_ROW_DIM<bf16>)); 
+            // if constexpr (is_causal) {
+            //     const int q_blk = (seq_idx * (K::qo_height/kittens::TILE_ROW_DIM<bf16>)) + warpid; 
+            //           int k_blk = (kv_idx  * (K::kv_height/kittens::TILE_ROW_DIM<bf16>)); 
 
-                #pragma unroll
-                for(int _ = 0; k_blk == (kv_iters-1)*(K::kv_height/kittens::TILE_ROW_DIM<bf16>) || k_blk == (kv_iters)*(K::kv_height/kittens::TILE_ROW_DIM<bf16>); k_blk+=10000) {
-                    #pragma unroll
-                    for (auto j = 0; j < (K::kv_height/kittens::TILE_ROW_DIM<bf16>); j++) {
-                        auto k_idx         = k_blk + j;
-                        auto &attn_subtile = reinterpret_cast<rt_fl<16, 16>&>(att_block.tiles[0][j]);
+            //     #pragma unroll
+            //     for(int _ = 0; k_blk == (kv_iters-1)*(K::kv_height/kittens::TILE_ROW_DIM<bf16>) || k_blk == (kv_iters)*(K::kv_height/kittens::TILE_ROW_DIM<bf16>); k_blk+=10000) {
+            //         #pragma unroll
+            //         for (auto j = 0; j < (K::kv_height/kittens::TILE_ROW_DIM<bf16>); j++) {
+            //             auto k_idx         = k_blk + j;
+            //             auto &attn_subtile = reinterpret_cast<rt_fl<16, 16>&>(att_block.tiles[0][j]);
 
-                        if      (k_idx >  q_blk) { neg_infty  (attn_subtile); }
-                        else if (k_idx == q_blk) { make_causal(attn_subtile, attn_subtile, kittens::base_types::constants<float>::neg_infty()); }
-                        __syncwarp();
-                    }
-                }
-            }
+            //             if      (k_idx >  q_blk) { neg_infty  (attn_subtile); }
+            //             else if (k_idx == q_blk) { make_causal(attn_subtile, attn_subtile, kittens::base_types::constants<float>::neg_infty()); }
+            //             __syncwarp();
+            //         }
+            //     }
+            // }
 
             row_max(max_vec, att_block, max_vec);
-            
+
             if constexpr (D == 64) { 
-                mul(att_block,      att_block, 1.44269504089f*0.125f); 
-                mul(max_vec_scaled, max_vec,   1.44269504089f*0.125f);
+                mul(att_block,      att_block, 1.44269504089f*g.softmax_scale);
+                mul(max_vec_scaled, max_vec,   1.44269504089f*g.softmax_scale);
             }
             else                   { 
-                mul(att_block,      att_block,  1.44269504089f*0.08838834764f); 
-                mul(max_vec_scaled, max_vec,    1.44269504089f*0.08838834764f);
+                mul(att_block,      att_block,  1.44269504089f*g.softmax_scale); 
+                mul(max_vec_scaled, max_vec,    1.44269504089f*g.softmax_scale);
             }
+            
+            // if constexpr (D == 64) { 
+            //     mul(att_block,      att_block, 1.44269504089f*0.125f); 
+            //     mul(max_vec_scaled, max_vec,   1.44269504089f*0.125f);
+            // }
+            // else                   { 
+            //     mul(att_block,      att_block,  1.44269504089f*0.08838834764f); 
+            //     mul(max_vec_scaled, max_vec,    1.44269504089f*0.08838834764f);
+            // }
 
             sub_row(att_block, att_block, max_vec_scaled);
             exp2(att_block, att_block);
@@ -198,7 +239,7 @@ void fwd_attend_ker(const __grid_constant__ fwd_globals<D> g) {
 
             wait(v_smem_arrived[(kv_idx)%K::stages], (kv_idx/K::stages)%2); 
 
-            warpgroup::mma_AB(o_reg, att_block_mma, v_smem[(kv_idx)%K::stages]);
+            warpgroup::mma_AB(o_reg, att_block_mma, k1_smem[(kv_idx)%K::stages]);
             warpgroup::mma_async_wait();
 
             if(warpgroup::laneid() == 0) arrive(compute_done[(kv_idx)%K::stages], 1);
@@ -208,8 +249,12 @@ void fwd_attend_ker(const __grid_constant__ fwd_globals<D> g) {
         warpgroup::store(o_smem[warpgroupid], o_reg);
         warpgroup::sync(warpgroupid+4);
 
+        // if (warpid % 4 == 0) {
+        //     coord<o_tile> o_tile_idx = {blockIdx.z, blockIdx.y, (seq_idx) + warpgroupid, 0};
+        //     tma::store_async(g.o, o_smem[warpgroupid], o_tile_idx);
+        // }
         if (warpid % 4 == 0) {
-            coord<o_tile> o_tile_idx = {blockIdx.z, blockIdx.y, (seq_idx) + warpgroupid, 0};
+            coord<o_tile> o_tile_idx = {blockIdx.z, 0, blockIdx.y, 0};
             tma::store_async(g.o, o_smem[warpgroupid], o_tile_idx);
         }
 

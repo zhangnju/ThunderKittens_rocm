3a9dee5b927f9e419bf14c5035e85a270fa9e7d8
Dan Fu <danfu09@gmail.com>
2025-03-28 15:27:11 -0700

    Benchmarking backwards

--- kernels/attn/b200/test_attn_pytorch.py
+++ kernels/attn/b200/test_attn_pytorch.py
@@ -98,4 +98,27 @@ compare_grads(q_grad_ref, qg, 'Q')
 compare_grads(k_grad_ref, kg, 'K')
 compare_grads(v_grad_ref, vg, 'V')
 
+qg = torch.zeros_like(q, dtype=torch.float, device='cuda')
+kg = torch.zeros_like(k, dtype=torch.float, device='cuda')
+vg = torch.zeros_like(v, dtype=torch.float, device='cuda')
+
+# Create d vector tensor
+d_vec = torch.empty((B, H, 1, N), dtype=torch.float, device='cuda')
+b200_attn.bwd_attend_prep_ker_128(o_grad, o, d_vec)
+b200_attn.bwd_attend_ker_128_noncausal(q, k, v, o_grad, qg, kg, vg, l, d_vec, q.shape[-2], 1)
+torch.cuda.synchronize()
+print('Finished warmup backwards')
+t0 = time.time()
+ITERS = 5
+for _ in range(ITERS):
+    b200_attn.bwd_attend_prep_ker_128(o_grad, o, d_vec)
+    b200_attn.bwd_attend_ker_128_noncausal(q, k, v, o_grad, qg, kg, vg, l, d_vec, q.shape[-2], 1)
+torch.cuda.synchronize()
+t1 = time.time()
+flops = 12 * B * H * N * N * D
+avg_us = (t1 - t0)/ITERS*1e6
+print(f'Time taken: {avg_us} us/iter')
+print(f'Total FLOPs: {flops}')
+print(f'TFLOPS: {(flops / (avg_us*1e-6))*10**-12} TFLOPS')
+
 breakpoint()

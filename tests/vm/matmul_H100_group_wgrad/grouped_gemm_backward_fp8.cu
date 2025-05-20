499e31bc4c639241bcb032508fade0237c7728d3
Stuart Sul <ssstuartss@gmail.com>
2025-05-19 21:42:14 -0500

    setup directory for grouped gemm backward

---
+++ tests/vm/matmul_H100_group_wgrad/grouped_gemm_backward_fp8.cu
@@ -0,0 +1,236 @@
+#include "kittens.cuh"
+#include "pyutils/pyutils.cuh"
+#include "vm/vm.cuh"
+#include <iostream>
+
+using namespace kittens;
+using namespace kittens::prototype;
+using namespace kittens::prototype::vm;
+
+constexpr int M_BLOCK = 128;
+constexpr int N_BLOCK = 256;
+constexpr int K_BLOCK = 128;
+
+using a_tile = st_fp8e4m3<M_BLOCK / 2, K_BLOCK>; // 2 consumer warpgroups
+using b_tile = st_fp8e4m3<N_BLOCK, K_BLOCK>;
+using c_tile = st_fp8e4m3<M_BLOCK / 2, N_BLOCK>;
+
+static constexpr int SM_COUNT = 132;
+static constexpr int NUM_GROUPS = 16; // must be decided at compile-time
+
+template<ducks::gl::all _GL, size_t _N>
+struct gl_array {
+    using GL = _GL;
+    static constexpr int N = _N;
+
+    GL gls[N];
+
+    __host__ inline gl_array(pybind11::list l) : 
+        gl_array(std::make_index_sequence<N>{}, l) { }
+
+    template<size_t... I> __host__ inline gl_array(std::index_sequence<I...>, pybind11::list l) : 
+        gls{py::from_object<GL>::make(l[I])...} { }
+
+    __host__ __device__ GL& operator[](size_t i) { return gls[i]; }
+    __host__ __device__ const GL& operator[](size_t i) const { return gls[i]; }
+};
+
+using config = default_config;
+struct globals {
+    using instruction_layout = ::kittens::prototype::vm::instruction_layout<config>;
+    using timing_layout = ::kittens::prototype::vm::timing_layout<config>;
+    using fp8_matrix = gl<fp8e4m3, 1, -1, -1, -1, a_tile, b_tile, c_tile>;
+    instruction_layout instructions;
+    timing_layout timings;
+    gl_array<fp8_matrix, NUM_GROUPS> A, B, C;
+    dim3 grid() { return dim3(SM_COUNT); }
+    dim3 block() { return dim3(config::NUM_THREADS); }
+    int dynamic_shared_memory() { return config::DYNAMIC_SHARED_MEMORY; }
+};
+
+template<typename config=config> struct GroupedGemmBackwardOp {
+    static constexpr int opcode = 1;
+    static constexpr int PIPELINE_STAGES = 4;
+
+    static_assert(config::NUM_PAGES == 13);
+    static_assert(config::PAGE_SIZE == 16384);
+    static_assert(PIPELINE_STAGES >= 2);
+    static_assert(config::NUM_CONSUMER_WARPS == 8);
+    static_assert(sizeof(a_tile) == config::PAGE_SIZE / 2);
+    static_assert(sizeof(b_tile) == config::PAGE_SIZE * 2);
+    static_assert(sizeof(c_tile) == config::PAGE_SIZE);
+    
+    struct parsed_instruction {
+        int group_id, row, col, num_iters;
+        __device__ inline parsed_instruction(typename config::instruction_t &instruction) {
+            /*
+                Instruction format:
+                [0] = opcode
+                [1] = Group ID
+                [2] = Row offset of C, in units of M_BLOCK
+                [3] = Col offset of C, in units of N_BLOCK
+                [4] = K reduction dimension, in units of K_BLOCK
+            */
+            group_id = instruction[1];
+            row = instruction[2];
+            col = instruction[3];
+            num_iters = instruction[4];
+        }
+        __device__ inline parsed_instruction(state<config> &s): parsed_instruction(s.instruction()) {}
+    };
+
+    __device__ static inline semaphore &inputs_arrived(state<config> &s, int stage) { return s.semaphores()[stage]; }
+    __device__ static inline semaphore &inputs_finished(state<config> &s, int stage) { return s.semaphores()[stage+PIPELINE_STAGES]; }
+    __device__ static inline semaphore &outputs_arrived(state<config> &s, int consumer_id) { return s.semaphores()[consumer_id+PIPELINE_STAGES*2]; }
+    __device__ static inline int get_a_page(state<config> &s, int stage) { return stage*3; }
+    __device__ static inline int get_b_page(state<config> &s, int stage) { return stage*3 + 1; }
+    __device__ static inline int get_store_page(state<config> &s, int consumer_id) {
+        return (PIPELINE_STAGES-1)*3 + consumer_id; // use 2 pages from the last stage
+    }
+
+    struct controller {
+        static __device__ int release_lid(const globals &g, typename config::instruction_t &instruction, int &query) {
+            return query;
+        }
+        static __device__ int init_semaphores(const globals &g, state<config> &s) {
+            for(int i = 0; i < PIPELINE_STAGES; i++) {
+                init_semaphore(inputs_arrived(s, i), 1);
+                init_semaphore(inputs_finished(s, i), 2);
+            }
+            for(int i = 0; i < 2; i++) {
+                init_semaphore(outputs_arrived(s, i), 4);
+            }
+            return 2*PIPELINE_STAGES + 2;
+        }
+    };
+    struct loader {
+        static __device__ void run(const globals &g, state<config> &s) {
+            parsed_instruction inst{s};
+            int laneid = warp::laneid();
+
+            if (laneid >= PIPELINE_STAGES*3 && laneid < config::NUM_PAGES) {
+                s.wait_page_ready(laneid);
+                s.finish_page(laneid, config::NUM_CONSUMER_WARPS); // release unused pages immediately
+            }
+
+            int pipeline_stage = 0;
+            uint32_t semaphore_bitfield = 0xFFFF0000; // ***_finished phase bits start as 1s, ***_arrived phase bits start as 0s
+            for (int i = 0; i < inst.num_iters; i++, pipeline_stage=ring_advance<PIPELINE_STAGES>(pipeline_stage)) {
+                wait(inputs_finished(s, pipeline_stage), get_phasebit<1>(semaphore_bitfield, pipeline_stage));
+                warp::tma::expect_bytes(inputs_arrived(s, pipeline_stage), sizeof(a_tile)*2 + sizeof(b_tile));
+                __syncwarp(); // none-0 lanes must wait for the tma::expect_bytes to complete
+                if (laneid < 2) {
+                    int a_page = get_a_page(s, pipeline_stage);
+                    if (i < PIPELINE_STAGES) {
+                        s.wait_page_ready(a_page);
+                    }
+                    a_tile &a = *reinterpret_cast<a_tile *>((uint8_t *)s.pages[a_page].data + sizeof(a_tile) * laneid);
+                    tma::load_async(a, g.A[inst.group_id], {inst.row*2 + laneid, i}, inputs_arrived(s, pipeline_stage));
+                } else if (laneid == 2) {
+                    int b_page = get_b_page(s, pipeline_stage);
+                    if (i < PIPELINE_STAGES) {
+                        s.wait_page_ready(b_page);
+                        s.wait_page_ready(b_page + 1); // because b_page is a megapage
+                    }
+                    b_tile &b = *reinterpret_cast<b_tile *>(s.pages[b_page].data);
+                    tma::load_async(b, g.B[inst.group_id], {inst.col, i}, inputs_arrived(s, pipeline_stage));
+                }
+                update_phasebit<1>(semaphore_bitfield, pipeline_stage);
+            }
+            warp::sync();
+
+            for (int i = 0; i < PIPELINE_STAGES; i++, pipeline_stage=ring_advance<PIPELINE_STAGES>(pipeline_stage)) {
+                wait(inputs_finished(s, pipeline_stage), get_phasebit<1>(semaphore_bitfield, pipeline_stage));
+                if ((laneid < 3 && pipeline_stage < PIPELINE_STAGES - 1) || (laneid == 2 && pipeline_stage == PIPELINE_STAGES - 1)) {
+                    int release_pid = pipeline_stage*3 + laneid;
+                    s.finish_page(release_pid, config::NUM_CONSUMER_WARPS);
+                }
+            }
+        }
+    };
+    struct launcher { // no warpgroup-level tensor cores in H100s
+        static __device__ void run(const globals &g, state<config> &s) { }
+    };
+    struct consumer {
+        static __device__ void run(const globals &g, state<config> &s) {
+            parsed_instruction inst{s};
+            int laneid = warpgroup::laneid();
+            int warpid = warpgroup::warpid();
+            int groupid = warpgroup::groupid();
+
+            rt_fl<M_BLOCK / config::NUM_CONSUMER_WARPS, N_BLOCK> acc_fl;
+            warp::zero(acc_fl);
+
+            int pipeline_stage = 0;
+            uint32_t semaphore_bitfield = 0xFFFF0000; // ***_finished phase bits start as 1s, ***_arrived phase bits start as 0s
+            for (int i = 0; i < inst.num_iters; i++, update_phasebit<0>(semaphore_bitfield, pipeline_stage), pipeline_stage=ring_advance<PIPELINE_STAGES>(pipeline_stage)) {
+                wait(inputs_arrived(s, pipeline_stage), get_phasebit<0>(semaphore_bitfield, pipeline_stage));
+                int a_page = get_a_page(s, pipeline_stage);
+                int b_page = get_b_page(s, pipeline_stage);
+                a_tile &a = *reinterpret_cast<a_tile *>((uint8_t *)s.pages[a_page].data + sizeof(a_tile) * groupid);
+                b_tile &b = *reinterpret_cast<b_tile *>(s.pages[b_page].data);
+                warpgroup::mma_ABt(acc_fl, a, b);
+                warpgroup::mma_async_wait();
+                warpgroup::arrive(inputs_finished(s, pipeline_stage));
+            }
+
+            rt_fp8e4m3<M_BLOCK / config::NUM_CONSUMER_WARPS, N_BLOCK> acc_fp8;
+            warp::copy(acc_fp8, acc_fl);
+
+            int store_page = get_store_page(s, groupid);
+            c_tile &store_buffer = *reinterpret_cast<c_tile *>(s.pages[store_page].data);
+            group<config::NUM_CONSUMER_WARPS>::sync(1); // must sync as we reuse the pages from the last stage
+            warpgroup::store(store_buffer, acc_fp8);
+            __syncwarp();
+            warp::arrive(outputs_arrived(s, groupid));
+        }
+    };
+    struct storer {
+        static __device__ void run(const globals &g, state<config> &s) {
+            parsed_instruction inst{s};
+            int laneid = warp::laneid();
+
+            if (laneid < 2) {
+                wait(outputs_arrived(s, laneid), 0);
+                int store_page = get_store_page(s, laneid);
+                c_tile &store_buffer = *reinterpret_cast<c_tile *>(s.pages[store_page].data);
+                tma::store_async(g.C[inst.group_id], store_buffer, {inst.row*2 + laneid, inst.col});
+            }
+            __syncwarp();
+            if (laneid == 0) tma::store_async_read_wait();
+            __syncwarp();
+            if (laneid < 2) {
+                int store_page = get_store_page(s, laneid);
+                s.finish_page(store_page, config::NUM_CONSUMER_WARPS);
+            }
+        }
+    };
+};
+
+PYBIND11_MODULE(grouped_gemm_backward_fp8, m) {
+    m.doc() = "grouped_gemm_backward_fp8 python module";
+    m.def("grouped_gemm_backward_fp8", [](
+        pybind11::object instructions,
+        pybind11::object timings,
+        pybind11::object A,
+        pybind11::object B,
+        pybind11::object C,
+        pybind11::kwargs kwargs
+    ) {
+        globals __g__{
+            py::from_object<typename globals::instruction_layout>::make(instructions),
+            py::from_object<typename globals::timing_layout>::make(timings),
+            gl_array<typename globals::fp8_matrix, NUM_GROUPS>(pybind11::cast<pybind11::list>(A)),
+            gl_array<typename globals::fp8_matrix, NUM_GROUPS>(pybind11::cast<pybind11::list>(B)),
+            gl_array<typename globals::fp8_matrix, NUM_GROUPS>(pybind11::cast<pybind11::list>(C))
+        };
+        cudaStream_t raw_stream = nullptr;
+        if (kwargs.contains("stream")) {
+            uintptr_t stream_ptr = kwargs["stream"].attr("cuda_stream").cast<uintptr_t>();
+            raw_stream = reinterpret_cast<cudaStream_t>(stream_ptr);
+        }
+        int __dynamic_shared_memory__ = (int)__g__.dynamic_shared_memory();
+        cudaFuncSetAttribute(kvm<config, globals, GroupedGemmBackwardOp<config>>, cudaFuncAttributeMaxDynamicSharedMemorySize, __dynamic_shared_memory__);
+        kvm<config, globals, GroupedGemmBackwardOp<config>><<<__g__.grid(), __g__.block(), __dynamic_shared_memory__, raw_stream>>>(__g__);
+    });
+}
---
+++ tests/vm/matmul_H100_group_wgrad/test.py
@@ -0,0 +1,115 @@
+import torch
+from grouped_gemm_backward_fp8 import grouped_gemm_backward_fp8
+
+# Must match that of grouped_gemm_backward_fp8.cu
+NUM_GROUPS = 16 
+
+# In the order of M, K, N
+# Keep K in multiples of 512 for best performance
+# For now, K must be >= 2048
+GROUP_SHAPES = [
+    (3072, 8192, 4096),
+    (4096, 16384*3, 8192),
+    (8192, 8192, 8192),
+    (3072, 16384*2, 3072),
+    (8192, 16384, 8192),
+    (8192, 8192, 16384),
+    (8192, 16384, 8192),
+    (4096, 8192, 3072),
+    (8192, 16384*3, 8192),
+    (16384, 16384, 16384),
+    (8192, 8192, 8192),
+    (3072, 16384*2, 3072),
+    (8192, 16384, 8192),
+    (4096, 4096, 4096),
+    (16384, 16384, 4096),
+    (8192, 16384, 8192),
+]
+
+OPCODE = 1
+SM_COUNT = 132
+INSTRUCTION_WIDTH = 32
+TIMING_WIDTH = 128
+NUM_ITERS = 10
+NUM_WARMUP_ITERS = 3
+
+M_BLOCK = 128
+K_BLOCK = 128
+N_BLOCK = 256
+SUPER_M = 1024 # for better L2 cache utilization
+
+assert len(GROUP_SHAPES) == NUM_GROUPS
+for M, K, N in GROUP_SHAPES:
+    if M%M_BLOCK != 0: raise ValueError(f'M must be divisible by {M_BLOCK}')
+    if K%K_BLOCK != 0: raise ValueError(f'K must be divisible by {K_BLOCK}')
+    if N%N_BLOCK != 0: raise ValueError(f'N must be divisible by {N_BLOCK}')
+
+# Create input and output tensors
+print('Starting test...')
+torch.manual_seed(1)
+A = [(torch.randn((M, K), device=0, dtype=torch.float32) / K**.25).to(torch.float8_e4m3fn) for M, K, N in GROUP_SHAPES]
+B = [(torch.randn((N, K), device=0, dtype=torch.float32) / K**.25).to(torch.float8_e4m3fn) for M, K, N in GROUP_SHAPES]
+C = [torch.zeros((M, N), device=0, dtype=torch.float8_e4m3fn) for M, K, N in GROUP_SHAPES]
+print('Input tensors created')
+print('A shapes:', [a.shape for a in A])
+print('B shapes:', [b.shape for b in B])
+print('C shapes:', [c.shape for c in C])
+
+instruction_idx = 0
+instructions = [[] for _ in range(SM_COUNT)]
+for group_id, (M, K, N) in enumerate(GROUP_SHAPES):
+    num_iters = K // K_BLOCK
+    for row_outer in range((M + SUPER_M - 1) // SUPER_M): # ceil
+        for col in range(N // N_BLOCK):
+            rows_per_super = SUPER_M // M_BLOCK
+            row_start = rows_per_super * row_outer
+            for row_inner in range(rows_per_super):
+                row = row_start + row_inner
+                if row >= M // M_BLOCK:
+                    break
+                instructions[instruction_idx%SM_COUNT].append([OPCODE, group_id, row, col, num_iters] + [0]*(INSTRUCTION_WIDTH-5))
+                instruction_idx += 1
+
+# Pad instructions
+max_instructions = -1
+for i in range(SM_COUNT):
+    max_instructions = max(max_instructions, len(instructions[i]))
+for i in range(SM_COUNT):
+    while len(instructions[i]) < max_instructions:
+        instructions[i].append([0] * INSTRUCTION_WIDTH)
+
+instructions = torch.tensor(instructions, dtype=torch.int32, device=0)
+timings = torch.zeros((SM_COUNT, instructions.shape[1], TIMING_WIDTH), dtype=torch.int32, device=0)
+print(f'Instruction and timing tensors created, of shapes {instructions.shape} and {timings.shape}')
+
+# Run the grouped_gemm_backward_fp8 kernel
+print('Launching kernel...')
+grouped_gemm_backward_fp8(instructions, timings, A, B, C)
+torch.cuda.synchronize()
+
+print('Starting timing loop...')
+for i in range(NUM_WARMUP_ITERS):
+    grouped_gemm_backward_fp8(instructions, timings, A, B, C)
+torch.cuda.synchronize()
+start_event = torch.cuda.Event(enable_timing=True)
+end_event = torch.cuda.Event(enable_timing=True)
+start_event.record()
+for i in range(NUM_ITERS):
+    grouped_gemm_backward_fp8(instructions, timings, A, B, C)
+torch.cuda.synchronize()
+end_event.record()
+torch.cuda.synchronize()
+elapsed_time = start_event.elapsed_time(end_event)
+sec_per_iter = ((elapsed_time / 1000) / NUM_ITERS)
+tflops = sum([2*M*K*N for M, K, N in GROUP_SHAPES]) * 1e-12
+print(f'Time per iter: {sec_per_iter * 1e6} us')
+print(f'TFLOP/s: {tflops/(sec_per_iter)}')
+
+print('Test completed successfully!')
+
+for group_id in range(NUM_GROUPS):
+    C_impl = C[group_id].to(torch.float32).cpu().numpy()
+    C_ref = (A[group_id].to(torch.float16) @ B[group_id].to(torch.float16).T).to(torch.float8_e4m3fn).to(torch.float32).cpu().numpy()
+    assert C_impl.shape == C_ref.shape
+    print(f'Group {group_id} abs diff max:', abs(C_impl - C_ref).max())
+    print(f'Group {group_id} abs diff mean:', abs(C_impl - C_ref).mean())
